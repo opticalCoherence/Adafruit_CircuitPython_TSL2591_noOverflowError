9f97e78234355989a21150285ea6290bc1b7833c
opticalCoherence <65328823+opticalCoherence@users.noreply.github.com>
2022-04-16 20:20:31 -0600

    Update adafruit_tsl2591.py
    Just commenting out the overflow exception so that the code does not break when the light sensor is hit with too much light

--- adafruit_tsl2591.py
+++ adafruit_tsl2591.py
@@ -277,12 +277,12 @@ class TSL2591:
             max_counts = _TSL2591_MAX_COUNT
 
         # Handle overflow.
-        if channel_0 >= max_counts or channel_1 >= max_counts:
-            message = (
-                "Overflow reading light channels!, Try to reduce the gain of\n "
-                + "the sensor using adafruit_tsl2591.GAIN_LOW"
-            )
-            raise RuntimeError(message)
+        #if channel_0 >= max_counts or channel_1 >= max_counts:
+        #    message = (
+        #        "Overflow reading light channels!, Try to reduce the gain of\n "
+        #        + "the sensor using adafruit_tsl2591.GAIN_LOW"
+        #    )
+        #    raise RuntimeError(message)
         # Calculate lux using same equation as Arduino library:
         #  https://github.com/adafruit/Adafruit_TSL2591_Library/blob/master/Adafruit_TSL2591.cpp
         again = 1.0
